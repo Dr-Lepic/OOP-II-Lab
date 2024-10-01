38f59c4553ddc541de623225ecd660d0e07b8d2d
Dr-Lepic <mbmahbub007@gmail.com>
2024-10-01 10:55:32 +0600

    Copied from lab 1 and created BookGrab interface

---
+++ src/Lab4/Book.java
@@ -0,0 +1,26 @@
+package Lab4;
+
+public class Book {
+    public String title;
+    public String author;
+    public String genre;
+    public int pages;
+    public Date dateFinished;
+    public Book(String title, String author, String genre, int pages, Date dateFinished) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.pages = pages;
+        this.dateFinished = dateFinished;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + " Author: "
+                + author + " Genre: " + genre + " Pages: " + pages;
+    }
+}
