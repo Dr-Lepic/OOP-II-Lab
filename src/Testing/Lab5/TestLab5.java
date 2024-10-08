cc45445c14ed8eeebf6330328fc5a3e48590ba74
Dr-Lepic <mbmahbub007@gmail.com>
2024-10-08 12:08:36 +0600

    tested

--- src/Testing/Lab5/TestLab5.java
+++ src/Testing/Lab5/TestLab5.java
@@ -17,8 +17,9 @@ public class TestLab5 {
     }
 
     @Test
-    public void test_Electronic_creation_invalid() {
-
+    public void test_Electronic_creation_price() {
+        Electronics test = new Electronics("Mouse", 20, 2.00);
+        assertEquals(2.00, test.getUnitPrice());
     }
 
     @Test
@@ -28,13 +29,20 @@ public class TestLab5 {
     }
 
     @Test
+    public void test_Clothing_creation_price() {
+        Clothing test = new Clothing("T-shirt", 10, 1.00);
+        assertEquals(1.00, test.getUnitPrice());
+    }
+
+
+    @Test
     public void test_Furniture_creation_name() {
         Furniture test = new Furniture("Table", 20, 200.00);
         assertEquals("Table", test.getName());
     }
 
     @Test
-    public void test_total(){
+    public void test_furniture_total(){
         Furniture test = new Furniture("Table", 20, 200.00);
         assertEquals(216, test.getTotalPrice());
     }
@@ -46,7 +54,7 @@ public class TestLab5 {
     }
 
     @Test
-    public void test_furniture_total(){
+    public void test_clothing_total(){
         Clothing test = new Clothing("Shirt", 20, 100.00);
         assertEquals(110, test.getTotalPrice());
     }
@@ -56,6 +64,13 @@ public class TestLab5 {
         MakeProduct test = new MakeProduct();
         assertEquals(9, test.productCount());
     }
+
+    @Test
+    public void test_Furniture_creation_stock() {
+        Furniture test = new Furniture("Table", 20, 200.00);
+        assertEquals(20, test.getStock());
+    }
+
 
 
 
