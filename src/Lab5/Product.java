2deaaabadd25298873ffb5e4959fe18f7d864ad1
Dr-Lepic <mbmahbub007@gmail.com>
2024-10-08 11:07:14 +0600

    modified the constructors

--- src/Lab5/Product.java
+++ src/Lab5/Product.java
@@ -4,11 +4,13 @@ public abstract class Product {
     String name;
     int stock;
     double unitPrice;
+    double totalPrice;
 
     public Product(String name, int stock, double unitPrice) {
         this.name = name;
         this.stock = stock;
         this.unitPrice = unitPrice;
+        this.totalPrice = this.calculateTotalPrice();
     }
 
     abstract double calculateTotalPrice();
