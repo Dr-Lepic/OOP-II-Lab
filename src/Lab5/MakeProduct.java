fab5a8ddea79bc7ba61fa953321ce3c2c81b64e4
Dr-Lepic <mbmahbub007@gmail.com>
2024-10-08 11:20:54 +0600

    Created MakeProduct class

---
+++ src/Lab5/MakeProduct.java
@@ -0,0 +1,22 @@
+package Lab5;
+
+import java.util.ArrayList;
+
+public class MakeProduct {
+    ArrayList<Product> createdProducts;
+    public MakeProduct() {
+        createdProducts = new ArrayList<>();
+    }
+
+    void generateProducts() {
+        createdProducts.add(new Electronics("Mouse", 20, 2.00));
+        createdProducts.add(new Electronics("Mouse", 10, 3.00));
+        createdProducts.add(new Electronics("Keyboard", 50, 2.00));
+        createdProducts.add(new Clothing("T-shirt", 10, 1.00));
+        createdProducts.add(new Clothing("T-shirt-2", 20, 1.00));
+        createdProducts.add(new Clothing("T-shirt-3", 10, 2.00));
+        createdProducts.add(new Furniture("Table", 20, 200.00));
+        createdProducts.add(new Furniture("Chair", 10, 140.00));
+        createdProducts.add(new Furniture("Chair-2", 20, 130.00));
+    }
+}
