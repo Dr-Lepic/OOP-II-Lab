c580311dc94fca3071109b510581cc0bfc1a1d6b
Dr-Lepic <mbmahbub007@gmail.com>
2024-10-08 11:07:34 +0600

    Created OrderManager class

---
+++ src/Lab5/OrderManager.java
@@ -0,0 +1,10 @@
+package Lab5;
+
+import java.util.ArrayList;
+
+public class OrderManager {
+    ArrayList<Product>products;
+    public OrderManager(ArrayList<Product> products) {
+        this.products = products;
+    }
+}
